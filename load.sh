44690de761dc2ce8e5fb508b94c6cffce33d8cb8
Arjit Chaudhary <arjit.c@gmail.com>
2013-06-16 21:48:38 +0530

    Create load.sh

---
+++ load.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+echo Load templates
+read -p "press [enter] to start..."
+cd /vz/template/cache
+wget http://download.openvz.org/template/precreated/centos-6-x86.tar.gz
+wget http://download.openvz.org/template/precreated/centos-6-x86_64.tar.gz
+wget http://download.openvz.org/template/precreated/debian-6.0-x86.tar.gz
+wget http://download.openvz.org/template/precreated/debian-6.0-x86_64.tar.gz
+wget http://download.openvz.org/template/precreated/ubuntu-12.04-x86.tar.gz
+wget http://download.openvz.org/template/precreated/ubuntu-12.04-x86_64.tar.gz
+wget http://download.openvz.org/template/precreated/ubuntu-12.10-x86.tar.gz
+wget http://download.openvz.org/template/precreated/ubuntu-12.10-x86_64.tar.gz
+clear
+echo Done.
+read -p "press [enter] to continue..."
