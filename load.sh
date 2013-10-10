0491b84db17d5bd363c6af205fd9e41709de0006
Arjit Chaudhary <arjit.c@gmail.com>
2013-10-10 20:56:48 +0530

    CentOS 5 / Ubuntu 13.04 added

--- load.sh
+++ load.sh
@@ -2,6 +2,8 @@
 echo Load templates
 read -p "press [enter] to start..."
 cd /vz/template/cache
+wget http://download.openvz.org/template/precreated/centos-5-x86.tar.gz
+wget http://download.openvz.org/template/precreated/centos-5-x86_64.tar.gz
 wget http://download.openvz.org/template/precreated/centos-6-x86.tar.gz
 wget http://download.openvz.org/template/precreated/centos-6-x86_64.tar.gz
 wget http://download.openvz.org/template/precreated/debian-6.0-x86.tar.gz
@@ -10,6 +12,8 @@ wget http://download.openvz.org/template/precreated/ubuntu-12.04-x86.tar.gz
 wget http://download.openvz.org/template/precreated/ubuntu-12.04-x86_64.tar.gz
 wget http://download.openvz.org/template/precreated/ubuntu-12.10-x86.tar.gz
 wget http://download.openvz.org/template/precreated/ubuntu-12.10-x86_64.tar.gz
+wget http://download.openvz.org/template/precreated/ubuntu-13.04-x86.tar.gz
+wget http://download.openvz.org/template/precreated/ubuntu-13.04-x86_64.tar.gz
 clear
 echo Done.
 read -p "press [enter] to continue..."
