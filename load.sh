c57a6f7f1816a01f887d3b10b6ad306758be9be0
Arjit Chaudhary <arjit.c@gmail.com>
2013-12-28 15:37:02 +0530

    wget -> wget -N

--- load.sh
+++ load.sh
@@ -2,20 +2,20 @@
 echo Load templates
 read -p "press [enter] to start..."
 cd /vz/template/cache
-wget http://download.openvz.org/template/precreated/centos-5-x86.tar.gz
-wget http://download.openvz.org/template/precreated/centos-5-x86_64.tar.gz
-wget http://download.openvz.org/template/precreated/centos-6-x86.tar.gz
-wget http://download.openvz.org/template/precreated/centos-6-x86_64.tar.gz
-wget http://download.openvz.org/template/precreated/debian-6.0-x86.tar.gz
-wget http://download.openvz.org/template/precreated/debian-6.0-x86_64.tar.gz
-wget http://download.openvz.org/template/precreated/ubuntu-12.04-x86.tar.gz
-wget http://download.openvz.org/template/precreated/ubuntu-12.04-x86_64.tar.gz
-wget http://download.openvz.org/template/precreated/ubuntu-12.10-x86.tar.gz
-wget http://download.openvz.org/template/precreated/ubuntu-12.10-x86_64.tar.gz
-wget http://download.openvz.org/template/precreated/ubuntu-13.04-x86.tar.gz
-wget http://download.openvz.org/template/precreated/ubuntu-13.04-x86_64.tar.gz
-wget http://download.openvz.org/template/precreated/debian-7.0-x86.tar.gz
-wget http://download.openvz.org/template/precreated/debian-7.0-x86_64.tar.gz
+wget -N http://download.openvz.org/template/precreated/centos-5-x86.tar.gz
+wget -N http://download.openvz.org/template/precreated/centos-5-x86_64.tar.gz
+wget -N http://download.openvz.org/template/precreated/centos-6-x86.tar.gz
+wget -N http://download.openvz.org/template/precreated/centos-6-x86_64.tar.gz
+wget -N http://download.openvz.org/template/precreated/debian-6.0-x86.tar.gz
+wget -N http://download.openvz.org/template/precreated/debian-6.0-x86_64.tar.gz
+wget -N http://download.openvz.org/template/precreated/ubuntu-12.04-x86.tar.gz
+wget -N http://download.openvz.org/template/precreated/ubuntu-12.04-x86_64.tar.gz
+wget -N http://download.openvz.org/template/precreated/ubuntu-12.10-x86.tar.gz
+wget -N http://download.openvz.org/template/precreated/ubuntu-12.10-x86_64.tar.gz
+wget -N http://download.openvz.org/template/precreated/ubuntu-13.04-x86.tar.gz
+wget -N http://download.openvz.org/template/precreated/ubuntu-13.04-x86_64.tar.gz
+wget -N http://download.openvz.org/template/precreated/debian-7.0-x86.tar.gz
+wget -N http://download.openvz.org/template/precreated/debian-7.0-x86_64.tar.gz
 clear
 echo Done.
 read -p "press [enter] to continue..."
