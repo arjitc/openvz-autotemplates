f3b415c525fbc8ab09394d5fee9b30214aa396d7
Arjit Chaudhary <arjit.c@gmail.com>
2014-06-28 14:28:25 +0530

    Added in minimal templates

--- load.sh
+++ load.sh
@@ -20,6 +20,15 @@ wget -N http://download.openvz.org/template/precreated/ubuntu-13.10-x86.tar.gz
 wget -N http://download.openvz.org/template/precreated/ubuntu-13.10-x86_64.tar.gz
 wget -N http://download.openvz.org/template/precreated/ubuntu-14.04-x86.tar.gz
 wget -N http://download.openvz.org/template/precreated/ubuntu-14.04-x86_64.tar.gz
+## minimal templates!
+http://download.openvz.org/template/precreated/centos-6-x86-minimal.tar.gz
+http://download.openvz.org/template/precreated/centos-6-x86_64-minimal.tar.gz
+http://download.openvz.org/template/precreated/debian-7.0-x86-minimal.tar.gz
+http://download.openvz.org/template/precreated/debian-7.0-x86_64-minimal.tar.gz
+http://download.openvz.org/template/precreated/ubuntu-12.04-x86-minimal.tar.gz
+http://download.openvz.org/template/precreated/ubuntu-12.04-x86_64-minimal.tar.gz
+http://download.openvz.org/template/precreated/ubuntu-14.04-x86-minimal.tar.gz
+http://download.openvz.org/template/precreated/ubuntu-14.04-x86_64-minimal.tar.gz
 clear
 echo Done.
 read -p "press [enter] to continue..."
