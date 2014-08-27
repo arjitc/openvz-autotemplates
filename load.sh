71dbd888d2b2dcd870c4834f020d6f4e439337f4
Arjit Chaudhary <arjit.c@gmail.com>
2014-08-27 19:47:33 +0530

    Added CentOS 7 x86_64 and Minimal x86_64

--- load.sh
+++ load.sh
@@ -20,6 +20,7 @@ wget -N http://download.openvz.org/template/precreated/ubuntu-13.10-x86.tar.gz
 wget -N http://download.openvz.org/template/precreated/ubuntu-13.10-x86_64.tar.gz
 wget -N http://download.openvz.org/template/precreated/ubuntu-14.04-x86.tar.gz
 wget -N http://download.openvz.org/template/precreated/ubuntu-14.04-x86_64.tar.gz
+wget -N http://download.openvz.org/template/precreated/centos-7-x86_64.tar.gz
 ## minimal templates!
 wget -N http://download.openvz.org/template/precreated/centos-6-x86-minimal.tar.gz
 wget -N http://download.openvz.org/template/precreated/centos-6-x86_64-minimal.tar.gz
@@ -29,6 +30,7 @@ wget -N http://download.openvz.org/template/precreated/ubuntu-12.04-x86-minimal.
 wget -N http://download.openvz.org/template/precreated/ubuntu-12.04-x86_64-minimal.tar.gz
 wget -N http://download.openvz.org/template/precreated/ubuntu-14.04-x86-minimal.tar.gz
 wget -N http://download.openvz.org/template/precreated/ubuntu-14.04-x86_64-minimal.tar.gz
+wget -N http://download.openvz.org/template/precreated/centos-7-x86_64-minimal.tar.gz
 clear
 echo Done.
 read -p "press [enter] to continue..."
