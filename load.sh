290a73a2bed965b4860a3c452c071b081b93cf40
Arjit Chaudhary <arjit.c@gmail.com>
2015-11-17 19:15:23 +0530

    Missed removing EOL template

--- load.sh
+++ load.sh
@@ -12,8 +12,6 @@ wget -N http://download.openvz.org/template/precreated/ubuntu-12.04-x86.tar.gz
 wget -N http://download.openvz.org/template/precreated/ubuntu-12.04-x86_64.tar.gz
 wget -N http://download.openvz.org/template/precreated/debian-7.0-x86.tar.gz
 wget -N http://download.openvz.org/template/precreated/debian-7.0-x86_64.tar.gz
-wget -N http://download.openvz.org/template/precreated/ubuntu-13.10-x86.tar.gz
-wget -N http://download.openvz.org/template/precreated/ubuntu-13.10-x86_64.tar.gz
 wget -N http://download.openvz.org/template/precreated/ubuntu-14.04-x86.tar.gz
 wget -N http://download.openvz.org/template/precreated/ubuntu-14.04-x86_64.tar.gz
 wget -N http://download.openvz.org/template/precreated/centos-7-x86_64.tar.gz
