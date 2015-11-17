3aa66b534e7676edfa5d448ce44249a219821847
Arjit Chaudhary <arjit.c@gmail.com>
2015-11-17 19:14:55 +0530

    Removed EOL templates added in Ubuntu 15.04

--- load.sh
+++ load.sh
@@ -10,10 +10,6 @@ wget -N http://download.openvz.org/template/precreated/debian-6.0-x86.tar.gz
 wget -N http://download.openvz.org/template/precreated/debian-6.0-x86_64.tar.gz
 wget -N http://download.openvz.org/template/precreated/ubuntu-12.04-x86.tar.gz
 wget -N http://download.openvz.org/template/precreated/ubuntu-12.04-x86_64.tar.gz
-wget -N http://download.openvz.org/template/precreated/ubuntu-12.10-x86.tar.gz
-wget -N http://download.openvz.org/template/precreated/ubuntu-12.10-x86_64.tar.gz
-wget -N http://download.openvz.org/template/precreated/ubuntu-13.04-x86.tar.gz
-wget -N http://download.openvz.org/template/precreated/ubuntu-13.04-x86_64.tar.gz
 wget -N http://download.openvz.org/template/precreated/debian-7.0-x86.tar.gz
 wget -N http://download.openvz.org/template/precreated/debian-7.0-x86_64.tar.gz
 wget -N http://download.openvz.org/template/precreated/ubuntu-13.10-x86.tar.gz
@@ -21,6 +17,7 @@ wget -N http://download.openvz.org/template/precreated/ubuntu-13.10-x86_64.tar.g
 wget -N http://download.openvz.org/template/precreated/ubuntu-14.04-x86.tar.gz
 wget -N http://download.openvz.org/template/precreated/ubuntu-14.04-x86_64.tar.gz
 wget -N http://download.openvz.org/template/precreated/centos-7-x86_64.tar.gz
+wget -N http://download.openvz.org/template/precreated/ubuntu-15.04-x86_64.tar.gz
 ## minimal templates!
 wget -N http://download.openvz.org/template/precreated/centos-6-x86-minimal.tar.gz
 wget -N http://download.openvz.org/template/precreated/centos-6-x86_64-minimal.tar.gz
@@ -31,6 +28,7 @@ wget -N http://download.openvz.org/template/precreated/ubuntu-12.04-x86_64-minim
 wget -N http://download.openvz.org/template/precreated/ubuntu-14.04-x86-minimal.tar.gz
 wget -N http://download.openvz.org/template/precreated/ubuntu-14.04-x86_64-minimal.tar.gz
 wget -N http://download.openvz.org/template/precreated/centos-7-x86_64-minimal.tar.gz
+wget -N http://download.openvz.org/template/precreated/ubuntu-15.04-x86_64-minimal.tar.gz
 clear
 echo Done.
 read -p "press [enter] to continue..."
