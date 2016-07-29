1f211971a184f3500323f6da0509798243129111
Arjit Chaudhary <arjit.c@gmail.com>
2016-07-29 19:30:17 +0530

    Remove Ubuntu 15.04 EOL

--- load.sh
+++ load.sh
@@ -13,7 +13,6 @@ wget -N http://download.openvz.org/template/precreated/debian-7.0-x86_64.tar.gz
 wget -N http://download.openvz.org/template/precreated/ubuntu-14.04-x86.tar.gz
 wget -N http://download.openvz.org/template/precreated/ubuntu-14.04-x86_64.tar.gz
 wget -N http://download.openvz.org/template/precreated/centos-7-x86_64.tar.gz
-wget -N http://download.openvz.org/template/precreated/ubuntu-15.04-x86_64.tar.gz
 wget -N http://download.openvz.org/template/precreated/debian-8.0-x86_64.tar.gz
 ## minimal templates!
 wget -N http://download.openvz.org/template/precreated/centos-6-x86-minimal.tar.gz
@@ -25,7 +24,6 @@ wget -N http://download.openvz.org/template/precreated/ubuntu-12.04-x86_64-minim
 wget -N http://download.openvz.org/template/precreated/ubuntu-14.04-x86-minimal.tar.gz
 wget -N http://download.openvz.org/template/precreated/ubuntu-14.04-x86_64-minimal.tar.gz
 wget -N http://download.openvz.org/template/precreated/centos-7-x86_64-minimal.tar.gz
-wget -N http://download.openvz.org/template/precreated/ubuntu-15.04-x86_64-minimal.tar.gz
 wget -N http://download.openvz.org/template/precreated/debian-8.0-x86_64-minimal.tar.gz
 clear
 echo Done.
