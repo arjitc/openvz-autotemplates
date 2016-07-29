b7c2b6f50c27baee79ae7f3cac34ae5d8de08d8a
Arjit Chaudhary <arjit.c@gmail.com>
2016-07-29 19:34:29 +0530

    Add ubuntu-16.04-x86_64

--- load.sh
+++ load.sh
@@ -14,6 +14,7 @@ wget -N http://download.openvz.org/template/precreated/ubuntu-14.04-x86.tar.gz
 wget -N http://download.openvz.org/template/precreated/ubuntu-14.04-x86_64.tar.gz
 wget -N http://download.openvz.org/template/precreated/centos-7-x86_64.tar.gz
 wget -N http://download.openvz.org/template/precreated/debian-8.0-x86_64.tar.gz
+wget -N http://download.openvz.org/template/precreated/ubuntu-16.04-x86_64.tar.gz
 ## minimal templates!
 wget -N http://download.openvz.org/template/precreated/centos-6-x86-minimal.tar.gz
 wget -N http://download.openvz.org/template/precreated/centos-6-x86_64-minimal.tar.gz
